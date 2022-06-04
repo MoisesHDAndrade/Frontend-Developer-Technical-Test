b4664618c96edcdde8c35c560d8f18bc018d9860
Moises Andrade <lawliex@gmail.com>
2022-06-04 17:34:50 +1200

    added toast message method

--- static/js/main.js
+++ static/js/main.js
@@ -6,12 +6,13 @@ let vue = new Vue({
         cart: [],
         selectedSize:'',
         sizesArray:[],
-        alertMessage: false
+        toastMessage: '',
+        toastType: '',
     },
     methods:{
         async getProducts(){
-            // const response = await axios.get('//localhost:5000/api');
-            const response = await axios.get('//sea-turtle-app-xz46v.ondigitalocean.app/api');
+            const response = await axios.get('//localhost:5000/api');
+            // const response = await axios.get('//sea-turtle-app-xz46v.ondigitalocean.app/api');
             this.products = response.data
         },
         selectSize(size){
@@ -25,15 +26,37 @@ let vue = new Vue({
                 "quantity":1
                 }
               
-              if(!this.sizesArray.includes(this.selectedSize.id)){
-                this.sizesArray.push(this.selectedSize.id)
-                this.cart.push(product)
-                
-              }else{
-                  this.cart.find(item => item.id === this.selectedSize.id).quantity++
-              }
+             if(this.selectedSize){
+                this.message(`${this.products.title} size ${this.selectedSize.label} was added to your cart`, 'success')
+                if(!this.sizesArray.includes(this.selectedSize.id)){
+                    this.sizesArray.push(this.selectedSize.id)
+                    this.cart.push(product)
+                    
+                  }else{
+                      this.cart.find(item => item.id === this.selectedSize.id).quantity++
+                  }
+             }
+             else{
+                this.message('Please select the size before add to cart' , 'danger')
+             }
+              
           
-        }
+        },
+        message(msg,type){
+            this.toastMessage = msg
+            this.toastType = type
+            this.messageToast()
+        },
+        messageToast(){
+            let toastLiveExample = document.getElementById('toast-message')
+            let toast = new bootstrap.Toast(toastLiveExample)
+            setTimeout(() => {
+                toast.show()
+            }), 500
+            // toast.show()
+            
+            
+        },
     },
     mounted(){
         this.getProducts()
